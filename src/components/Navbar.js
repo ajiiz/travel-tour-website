af594d31a8b1cd0940af835d28155230f5a7972b
ajiiz <opticpiter@gmail.com>
2020-12-01 21:55:00 +0100

    Added elements in NavBar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>
-
+        <div className="nav-bar">
+            <div className="nav-bar__logo">there goes logo</div>
+            <div className="nav-bar__menu">
+                <ul className="nav-bar__menu__items">
+                    <li className="nav-bar__menu__items__item">Home</li>
+                    <li className="nav-bar__menu__items__item">Destinations</li>
+                    <li className="nav-bar__menu__items__item">About</li>
+                    <li className="nav-bar__menu__items__item">Partner</li>
+                </ul>
+            </div>
+            <button className="btn btn-reversed">Login</button>
+            <button className="btn">Register</button>
         </div>
     )
 }
