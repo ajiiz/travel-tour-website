861b7ac3424fb7d5239675514e473a0f18881272
ajiiz <opticpiter@gmail.com>
2020-12-01 22:32:21 +0100

    Removed one parent element from menu and modified class names

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,12 @@ const Navbar = () => {
     return (
         <div className="nav-bar">
             <div className="nav-bar__logo">there goes logo</div>
-            <div className="nav-bar__menu">
-                <ul className="nav-bar__menu__items">
-                    <li className="nav-bar__menu__items__item">Home</li>
-                    <li className="nav-bar__menu__items__item">Destinations</li>
-                    <li className="nav-bar__menu__items__item">About</li>
-                    <li className="nav-bar__menu__items__item">Partner</li>
-                </ul>
-            </div>
+            <ul className="nav-bar__menu">
+                <li className="nav-bar__menu__item">Home</li>
+                <li className="nav-bar__menu__item">Destinations</li>
+                <li className="nav-bar__menu__item">About</li>
+                <li className="nav-bar__menu__item">Partner</li>
+            </ul>
             <button className="btn btn-reversed">Login</button>
             <button className="btn">Register</button>
         </div>
