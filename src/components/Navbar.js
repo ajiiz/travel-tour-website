f7828cb882bc4eebacff2913dc822f803404a766
ajiiz <opticpiter@gmail.com>
2020-12-02 12:39:56 +0100

    Added item--active class

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
         <div className="nav-bar">
             <div className="nav-bar__logo">there goes logo</div>
             <ul className="nav-bar__menu">
-                <li className="nav-bar__menu__item">Home</li>
+                <li className="nav-bar__menu__item nav-bar__menu__item--active">Home</li>
                 <li className="nav-bar__menu__item">Destinations</li>
                 <li className="nav-bar__menu__item">About</li>
                 <li className="nav-bar__menu__item">Partner</li>
