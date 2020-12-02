18e48fd5d8c7bb241c19afd617d8cd1556b69d5d
ajiiz <opticpiter@gmail.com>
2020-12-02 12:45:48 +0100

    Added new parent element to wrap menu and buttons in nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,16 @@ const Navbar = () => {
     return (
         <div className="nav-bar">
             <div className="nav-bar__logo">there goes logo</div>
-            <ul className="nav-bar__menu">
-                <li className="nav-bar__menu__item nav-bar__menu__item--active">Home</li>
-                <li className="nav-bar__menu__item">Destinations</li>
-                <li className="nav-bar__menu__item">About</li>
-                <li className="nav-bar__menu__item">Partner</li>
-            </ul>
-            <button className="btn btn-reversed">Login</button>
-            <button className="btn">Register</button>
+            <div className="nav-bar__menu">
+                <ul className="nav-bar__menu__items">
+                    <li className="nav-bar__menu__items__item nav-bar__menu__items__item--active">Home</li>
+                    <li className="nav-bar__menu__items__item">Destinations</li>
+                    <li className="nav-bar__menu__items__item">About</li>
+                    <li className="nav-bar__menu__items__item">Partner</li>
+                </ul>
+                <button className="nav-bar__menu__btn btn btn-reversed">Login</button>
+                <button className="nav-bar__menu__btn btn">Register</button>
+            </div>
         </div>
     )
 }
