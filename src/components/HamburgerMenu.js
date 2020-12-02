4a36f2fcd557dca7c057a003c9319f4b97ef3b30
ajiiz <opticpiter@gmail.com>
2020-12-02 20:27:12 +0100

    Added elements in HamburgerMenu components

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const HamburgerMenu = () => {
     return (
-        <div>
-            Hamburger
+        <div className="hamburger-menu">
+            <ul className="hamburger-menu__items">
+                <li className="hamburger-menu__items__item nav-bar__menu__items__item--active">Home</li>
+                <li className="hamburger-menu__items__item">Destinations</li>
+                <li className="hamburger-menu__items__item">About</li>
+                <li className="hamburger-menu__items__item">Partner</li>
+            </ul>
+            <button className="hamburger-menu__btn btn btn--reversed">Login</button>
+            <button className="hamburger-menu__btn btn">Register</button>
         </div>
     )
 }
