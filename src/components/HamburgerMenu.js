46a482d78b246caa6abecd5e17599d512d03bc6f
ajiiz <opticpiter@gmail.com>
2020-12-02 14:30:52 +0100

    Added HamburgerMenu component that will contain menu for mobile

---
+++ src/components/HamburgerMenu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HamburgerMenu = () => {
+    return (
+        <div>
+            Hamburger
+        </div>
+    )
+}
+
+export default HamburgerMenu
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import HamburgerMenu from './HamburgerMenu'
 
 const Navbar = () => {
     return (
