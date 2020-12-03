9d395296bf9f5fa0c846ee180a16cdc65e65bb51
ajiiz <opticpiter@gmail.com>
2020-12-03 14:06:23 +0100

    Added parent for elements in FinderButton Component

--- src/components/FinderButton.js
+++ src/components/FinderButton.js
@@ -3,10 +3,10 @@ import { BiChevronDown } from 'react-icons/bi'
 
 const FinderButton = ({ text }) => {
     return (
-        <>
-            <p className="header-info__finder__btns__text">{text}</p>
+        <div className="header-info__finder__btns__btn">
+            <p className="header-info__finder__btns__btn__text">{text}</p>
             <BiChevronDown />
-        </>
+        </div>
     )
 }
 
