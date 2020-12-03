93dac6cccd63fa606a4d07f30e2e6b516e65997b
ajiiz <opticpiter@gmail.com>
2020-12-03 12:27:25 +0100

    Added 2 header sections and imported image in Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import HeaderImage from '../assets/header.svg'
 
 const Header = () => {
     return (
-        <div>
-
-        </div>
+        <header>
+            <div className="header-info">
+                <div className="header-info__text">
+                    <h1 className="header-info__text__main">Explore and Travel</h1>
+                    <p className="header-info__text__lower">Holiday finder</p>
+                    <hr className="header-info__text__line"/>
+                </div>
+            </div>
+            <div className="header-image">
+                <img src={HeaderImage} alt="header image"></img>
+            </div>
+        </header>
     )
 }
 
