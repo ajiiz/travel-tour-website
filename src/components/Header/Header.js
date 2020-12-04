97f077ae257fb92e346e018ada20d24273bc75ce
ajiiz <opticpiter@gmail.com>
2020-12-04 21:56:09 +0100

    Changed alt text for image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
                 </div>
             </div>
             <div className="header-image">
-                <img src={HeaderImage} alt="header image"></img>
+                <img src={HeaderImage} alt="header"></img>
             </div>
         </header>
     )
