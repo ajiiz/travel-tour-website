e5e34d120f53ef1eb1e7d4292e8b280ae548fe5a
ajiiz <opticpiter@gmail.com>
2020-12-04 15:45:05 +0100

    Added file with article texts that will be used in props

---
+++ src/components/ArticleItems.js
@@ -0,0 +1,12 @@
+export const ArticleItems = [
+    {
+        header: "A new way to explore the world",
+        details: "For decades travellers have reached for Lonely Planet books when looking to plan and execute their percect trip, but now, they can also let Lonely Planet Experiences lead the way.",
+        btnText: "Learn More"
+    },
+    {
+        header: "Guides by Thousand Sunny",
+        details: "Packed with tips and advices from our on-the-ground experts, our city guides app (iOS and Android) that is ultimate resource before and during a trip",
+        btnText: "Download"
+    }
+]
