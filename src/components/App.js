9985c4b696fbe21be70a74ef7e99c1438f2b2ed1
ajiiz <opticpiter@gmail.com>
2020-12-04 15:36:53 +0100

    Imported Article images in App to use them as props

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import Footer from './Footer'
 import NavBar from './Navbar'
 import Testimonials from './Testimonials'
 import Trending from './Trending'
+import ArticleImage1 from '../assets/article-1.svg'
+import ArticleImage2 from '../assets/article-2.svg'
 
 const App = () => {
   return (
