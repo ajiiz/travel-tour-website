042af7036f8b320d9ea941b431e13013c1015760
ajiiz <opticpiter@gmail.com>
2020-12-04 22:04:03 +0100

    Destructurised and inserted props values in Article Component

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Article = () => {
+const Article = ({ image, header, details, btnText }) => {
     return (
         <div className="article">
             <div className="article__image">
-                <img src=""></img>
+                <img src={image} alt="article"></img>
             </div>
             <div className="article__info">
-                <h1 className="article__info__header"></h1>
-                <p className="article__info__details"></p>
-                <button className="btn"></button>
+                <h1 className="article__info__header">{header}</h1>
+                <p className="article__info__details">{details}</p>
+                <button className="btn">{btnText}</button>
             </div>
         </div>
     )
