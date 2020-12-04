54aa1b7764595e4c15815036633347e6e201ff32
ajiiz <opticpiter@gmail.com>
2020-12-04 22:54:07 +0100

    Added conditional class name setting depending on reversed prop value

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Article = ({ image, header, details, btnText }) => {
+const Article = ({ image, header, details, btnText, reversed}) => {
     return (
-        <div className="article">
+        <div className={(reversed) ? "article article--reversed" : "article"}>
             <div className="article__image">
                 <img src={image} alt="article"></img>
             </div>
-            <div className="article__info">
+            <div className={(reversed) ? "article__info article__info--reversed" : "article__info"}>
                 <h1 className="article__info__header">{header}</h1>
                 <p className="article__info__details">{details}</p>
                 <button className="btn">{btnText}</button>
