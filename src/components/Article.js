fd85c1a9869d1a18f4eaea0b81e7fd4097418eeb
ajiiz <opticpiter@gmail.com>
2020-12-04 15:34:09 +0100

    Added elements in Article Component

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Article = () => {
     return (
-        <div>
-
+        <div className="article">
+            <div className="article__image">
+                <img src=""></img>
+            </div>
+            <div className="article__info">
+                <h1 className="article__info__header"></h1>
+                <p className="article__info__details"></p>
+                <button className="btn"></button>
+            </div>
         </div>
     )
 }
