fa5af40dc6fb899a23e99af4f9e41af2f8d58b2c
ajiiz <opticpiter@gmail.com>
2020-12-07 19:47:58 +0100

    Imported components and data to Destinations Component and used map to render all items as DestinationItem Component with props

--- src/components/Destinations/Destinations.js
+++ src/components/Destinations/Destinations.js
@@ -1,14 +1,27 @@
 import React from 'react'
+import DestinationItem from "./DestinationItem"
+import Items from "./Items"
 
 const Destinations = () => {
     return (
-        <div clasName="destinations">
+        <div className="destinations">
             <div className="destinations__header">
                 <h1 className="destinations__header__main">Featured destinations</h1>
                 <p className="destinations__header__text">View all &gt;</p>
             </div>
             <div className="destinations__items">
-
+                {
+                    Items.map((data, key) => {
+                        return (
+                            <DestinationItem
+                                name={data.name}
+                                country={data.country}
+                                image={data.image}
+                                key={key}
+                            />
+                        )
+                    })
+                }
             </div>
         </div>
     )
