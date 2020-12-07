f586282a7fb259a22cbcde7ed8e6074cde6552ce
ajiiz <opticpiter@gmail.com>
2020-12-07 19:31:00 +0100

    Added elements to Destinations Component

--- src/components/Destinations/Destinations.js
+++ src/components/Destinations/Destinations.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Destinations = () => {
     return (
-        <div>
+        <div clasName="destinations">
+            <div className="destinations__header">
+                <h1 className="destinations__header__main">Featured destinations</h1>
+                <p className="destinations__header__text">View all &gt;</p>
+            </div>
+            <div className="destinations__items">
 
+            </div>
         </div>
     )
 }
