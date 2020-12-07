59091b347e1f0cce8bf8464c61b9b6f9176658ce
ajiiz <opticpiter@gmail.com>
2020-12-07 19:35:59 +0100

    Added Items in Destinations that contains all places with information

---
+++ src/components/Destinations/Items.js
@@ -0,0 +1,24 @@
+const Items = [
+    {
+        name: "Raja Ampat",
+        country: "Indonesia",
+        image: "dest-1.png"
+    },
+    {
+        name: "Fanjingshan",
+        country: "China",
+        image: "dest-2.png"
+    },
+    {
+        name: "Vevey",
+        country: "Switzerland",
+        image: "dest-3.png"
+    },
+    {
+        name: "Skadar",
+        country: "Montenegro",
+        image: "dest-4.png"
+    }
+]
+
+export default Items
