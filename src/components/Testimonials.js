93ef5e88686ca2ad2e1736d6a063f8350f2ee06e
ajiiz <opticpiter@gmail.com>
2020-12-07 21:20:12 +0100

    Added elements in Testimonials Component

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,8 +4,21 @@ import Stars from "../assets/stars.png"
 
 const Testimonials = () => {
     return (
-        <div>
-
+        <div className="testimonials">
+            <div className="testimonials__text">
+                <h1 className="testimonials__text__header">Testimonials</h1>
+                <img src={Stars} alt="stars"></img>
+                <p className="testimonials__text__quote">
+                    "Quosque in lacus a urna fementum euismod.
+                    Integer mi nibh, dapibus ac scelerisque eu,
+                    facilisis quis purus. Morbi blandit sit amet turpis nec."
+                </p>
+                <p className="testimonials__text__name">John Doe</p>
+                <p className="testimonials__text__info">Founder of Circle</p>
+            </div>
+            <div className="testimonials__image">
+                <img src={ProfileImage} alt="profile"></img>
+            </div>
         </div>
     )
 }
