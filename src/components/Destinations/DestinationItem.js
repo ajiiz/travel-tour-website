55fee810671ff38f7db492129a864f89bab91323
ajiiz <opticpiter@gmail.com>
2020-12-07 20:00:24 +0100

    Added elements with props in DestinationItem Component

--- src/components/Destinations/DestinationItem.js
+++ src/components/Destinations/DestinationItem.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const DestinationItems = ({ name, country, image }) => {
     return (
-        <div>
+        <div className="destinations__items__item">
+            <div className="destinations__items__item__image">
+                <img src={image} alt="destination"></img>
+            </div>
+            <div className="destinations__items__item__text-container">
+                <h1 className="destinations__items__item__text__name">{name}</h1>
+                <p className="destinations__items__item__text__country">{country}</p>
+            </div>
         </div>
     )
 }
