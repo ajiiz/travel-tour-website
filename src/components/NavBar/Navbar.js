070431ba2a30702d61be3de933a2eda4973c3040
ajiiz <opticpiter@gmail.com>
2020-12-08 22:08:04 +0100

    Added gsap animation for nav-bar

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import HamburgerMenu from './HamburgerMenu'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import Logo from './Logo'
+import { Power1, gsap } from 'gsap'
 
 const Navbar = () => {
 
     const [isHamburgerMenu, setIsHamburgerMenu] = useState(false);
+    let container = useRef(null)
+
+    useEffect(() => {
+        const menu = container.firstElementChild
+        const list = container.children[1]
+        const items = container.children[1].children[0]
+        gsap.from(menu, {duration: 2, y:"-250", ease:Power1.easeOut})
+        gsap.from(list, {duration: 2, y:"-250", ease: Power1.easeOut})
+        gsap.from(items, {delay: 2, duration: 1.5, opacity:0, ease: Power1.easeInOut, stagger: 0.5})
+    }, [])
 
     const handleClick = () => {
         setIsHamburgerMenu(!isHamburgerMenu)
@@ -13,7 +24,7 @@ const Navbar = () => {
 
     return (
         <>
-        <div className="nav-bar">
+        <div className="nav-bar" ref={el => container = el}>
             <Logo />
             <div className="nav-bar__menu">
                 <ul className="nav-bar__menu__items">
