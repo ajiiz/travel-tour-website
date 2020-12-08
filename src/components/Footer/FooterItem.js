1ada0c8a051643ec4e229b17a8fff86200fdb123
ajiiz <opticpiter@gmail.com>
2020-12-08 19:03:31 +0100

    Deleted one parent div so its all wrapper in only in the list in FooterItem Component, also fixed classNames

--- src/components/Footer/FooterItem.js
+++ src/components/Footer/FooterItem.js
@@ -2,16 +2,14 @@ import React from 'react'
 
 function FooterItem({ header, details }) {
     return (
-        <div className="footer__items__item">
-            <ul className="footer__items__item__list">
-                <li className="footer-_items__item__list__element footer-_items__item__list__element--header">{header}</li>
-                <li className="footer-_items__item__list__element">{details[0]}</li>
-                <li className="footer-_items__item__list__element">{details[1]}</li>
-                <li className="footer-_items__item__list__element">{details[2]}</li>
-                <li className="footer-_items__item__list__element">{details[3]}</li>
-                <li className="footer-_items__item__list__element">{details[4]}</li>
+        <ul className="footer__items__list">
+                <li className="footer__items__list__element footer__items__list__element--header">{header}</li>
+                <li className="footer__items__list__element">{details[0]}</li>
+                <li className="footer__items__list__element">{details[1]}</li>
+                <li className="footer__items__list__element">{details[2]}</li>
+                <li className="footer__items__list__element">{details[3]}</li>
+                <li className="footer__items__list__element">{details[4]}</li>
             </ul>
-        </div>
     )
 }
 
