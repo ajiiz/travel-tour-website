5288e8eea8935e2dc7e700e5e8d47176046e66c8
ajiiz <opticpiter@gmail.com>
2020-12-08 15:07:38 +0100

    Added left section elements in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Logo from "./NavBar/Logo"
 
 const Footer = () => {
     return (
-        <div>
-
+        <div className="footer">
+            <div className="footer__info">
+                <Logo />
+                <p className="footer__info__advice">
+                    Plan and book your perfect trip with expert advice,
+                    travel tips destination information from us
+                </p>
+                <p className="footer__info__copyright">
+                    ©2020 Thousand Sunny. All rights reserved
+                </p>
+            </div>
         </div>
     )
 }
