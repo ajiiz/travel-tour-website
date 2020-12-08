5d05a49ff40d5a208ee8b5d0b8c18b5ac70ea064
ajiiz <opticpiter@gmail.com>
2020-12-08 12:15:11 +0100

    Added elements in Trending Component

--- src/components/Trending/Trending.js
+++ src/components/Trending/Trending.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Trending = () => {
     return (
-        <div>
+        <div className="trending">
+            <div className="trending__header">
+                <h1 className="trending__header__main">Trending stories</h1>
+                <p className="trending__header__text">View all &gt;</p>
+            </div>
+            <div className="trnding__items">
 
+            </div>
         </div>
     )
 }
