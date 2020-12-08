1b9371bbc5a9adc10b585a7c5d0b3b753d2081f7
ajiiz <opticpiter@gmail.com>
2020-12-08 19:40:36 +0100

    Added mobile styling for footer and changed the order of elements in Footer Component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,16 +7,6 @@ import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn, FaYoutube } from "re
 const Footer = () => {
     return (
         <div className="footer">
-            <div className="footer__info">
-                <Logo />
-                <p className="footer__info__advice">
-                    Plan and book your perfect trip with expert advice,
-                    travel tips destination information from us
-                </p>
-                <p className="footer__info__copyright">
-                    ©2020 Thousand Sunny. All rights reserved
-                </p>
-            </div>
             <div className="footer__items">
                 {
                     Items.map((data, key) => {
@@ -29,6 +19,16 @@ const Footer = () => {
                         )
                     })
                 }
+            </div>
+            <div className="footer__info">
+                <Logo />
+                <p className="footer__info__advice">
+                    Plan and book your perfect trip with expert advice,
+                    travel tips destination information from us
+                </p>
+                <p className="footer__info__copyright">
+                    ©2020 Thousand Sunny. All rights reserved
+                </p>
             </div>
             <div className="footer__icons">
                 <FaTwitter className="footer__icons__icon" />
