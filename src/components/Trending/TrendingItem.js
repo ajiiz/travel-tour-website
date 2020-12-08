3200c09cb5fcdcb58b27fa6f506c7da6b5b74e08
ajiiz <opticpiter@gmail.com>
2020-12-08 12:24:38 +0100

    Added elements with props in TrendingItem Component

--- src/components/Trending/TrendingItem.js
+++ src/components/Trending/TrendingItem.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const TrendingItem = () => {
+const TrendingItem = ({ header, details, image }) => {
     return (
-        <div>
-
+        <div className="trending__items__item">
+            <div className="trending__items__item__image">
+                <img src={image} alt="trending"></img>
+            </div>
+            <div className="trending__items__item__text">
+                <h1 className="trending__items__item__text__header">{header}</h1>
+                <p className="trending__items__item__text__details">{details}</p>
+                <p className="trending__items-_item__text__info">Read more</p>
+            </div>
         </div>
     )
 }
