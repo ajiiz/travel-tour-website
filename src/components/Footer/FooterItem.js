d84aa2de3ffe8a51438ceeaf61ed7e14e338b895
ajiiz <opticpiter@gmail.com>
2020-12-08 18:34:50 +0100

    Added FooterItem elements and props

--- src/components/Footer/FooterItem.js
+++ src/components/Footer/FooterItem.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-function FooterItem() {
+function FooterItem({ header, details }) {
     return (
-        <div>
-
+        <div className="footer__items__item">
+            <ul className="footer__items__item__list">
+                <li className="footer-_items__item__list__element footer-_items__item__list__element--header">{header}</li>
+                <li className="footer-_items__item__list__element">{details[0]}</li>
+                <li className="footer-_items__item__list__element">{details[1]}</li>
+                <li className="footer-_items__item__list__element">{details[2]}</li>
+                <li className="footer-_items__item__list__element">{details[3]}</li>
+                <li className="footer-_items__item__list__element">{details[4]}</li>
+            </ul>
         </div>
     )
 }
