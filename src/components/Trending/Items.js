b2741f49a3654969d07a84cb5b51374729c7ac81
ajiiz <opticpiter@gmail.com>
2020-12-08 12:06:24 +0100

    Added Items that contain TrendingItems Component content that will be used as props

---
+++ src/components/Trending/Items.js
@@ -0,0 +1,29 @@
+import Image_1 from "../../assets/trend-1.png"
+import Image_2 from "../../assets/trend-2.png"
+import Image_3 from "../../assets/trend-3.png"
+import Image_4 from "../../assets/trend-4.png"
+
+const Items = [
+    {
+        header: "The many benefits of taking a healing holiday",
+        details: "'Healing Holidays' are on the rise to help maximise your health and happines...",
+        image: Image_1
+    },
+    {
+        header: "The best Kyoto restaurant to try Japanese food",
+        details: "From tofu to teahouses, here's our guide to Kyoto's best restaurants to visist...",
+        image: Image_2
+    },
+    {
+        header: "Skip Chichen Itza and head to this remote Yucatan",
+        details: "It's remote and challenging to get, but braving the jungle and exploring these ruins without the...",
+        image: Image_3
+    },
+    {
+        header: "Surf's up at these beginner spots around the world",
+        details: "If learning to surf has been on your to-do list for a while, the good news is: it's never too late...",
+        image: Image_4
+    }
+]
+
+export default Items
