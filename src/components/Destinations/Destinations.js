3a527de33052ad1be0db989171bd28d3d7695ed3
ajiiz <opticpiter@gmail.com>
2020-12-09 12:55:00 +0100

    Added destinations animation

--- src/components/Destinations/Destinations.js
+++ src/components/Destinations/Destinations.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import DestinationItem from "./DestinationItem"
 import Items from "./Items"
+import { Power1, gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 
 const Destinations = () => {
+
+    let container = useRef(null)
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger)
+        gsap.from(container.children,
+            {duration: 1.5, opacity: 0, ease: Power1.easeOut, stagger: 0.5,
+            scrollTrigger: {
+                trigger: container,
+                start: '-96 50%',
+            }
+        })
+    }, [])
+
     return (
-        <div className="destinations">
+        <div className="destinations" ref={el => container = el}>
             <div className="destinations__header">
                 <h1 className="destinations__header__main">Featured destinations</h1>
                 <p className="destinations__header__text">View all &gt;</p>
