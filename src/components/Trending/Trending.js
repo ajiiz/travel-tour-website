63933e9b88a53f4e1698f02b777dfb8ad3b4841d
ajiiz <opticpiter@gmail.com>
2020-12-09 12:59:40 +0100

    Added trending animation

--- src/components/Trending/Trending.js
+++ src/components/Trending/Trending.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import TrendingItem from "./TrendingItem"
 import Items from "./Items"
+import { Power1, gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 const Trending = () => {
+
+    let container = useRef(null)
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger)
+        gsap.from(container.children,
+            {duration: 1.5, opacity: 0, ease: Power1.easeOut, stagger: 0.5,
+            scrollTrigger: {
+                trigger: container,
+                start: '-96 50%',
+            }
+        })
+    }, [])
+
     return (
-        <div className="trending">
+        <div className="trending" ref={el => container = el}>
             <div className="trending__header">
                 <h1 className="trending__header__main">Trending stories</h1>
                 <p className="trending__header__text">View all &gt;</p>
